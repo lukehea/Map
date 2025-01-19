e6b0cc3f1e3dea79905a4f386fd06d04965d5c88
lukehea <lualhe416@gmail.com>
2025-01-19 17:52:17 -0500

    Add project files.

---
+++ Map/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Map
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MapBuilder map_builder;
+        private Point mouse_down_loc;
+        private ObservableCollection<ObservableCollection<Image>> image_grid;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            map_builder = new MapBuilder();
+
+            image_grid = new ObservableCollection<ObservableCollection<Image>>();
+            for(int i = 0; i < 4; ++i)
+            {
+                image_grid.Add(new ObservableCollection<Image>());
+                for(int j = 0; j < 4; ++j)
+                {
+                    image_grid[i].Add(new Image());
+                    Grid.SetColumn(image_grid[i][j], i);
+                    Grid.SetRow(image_grid[i][j], j);
+
+                   mapGrid.RegisterName($"image_{i}{j}", image_grid[i][j]);
+                   mapGrid.Children.Add(image_grid[i][j]);
+                }
+            }
+
+            updateMap();
+
+            updateLabels();
+        }
+
+        private void updateLabels()
+        {
+            latitudeLabel.Content = $"Latitude: {map_builder.getLatitude()}";
+            longitudeLabel.Content = $"Longitude: {map_builder.getLongitude()}";
+            zoomLabel.Content = $"Zoom: {map_builder.getZoom()}";
+        }
+
+        private void MainWindow_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            map_builder.updateZoom((e.Delta > 0) ? 1 : -1);
+            updateMap();
+            updateLabels();
+        }
+
+        private void mapGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Point drag_distance = (Point)((Point) e.GetPosition(this) - mouse_down_loc);
+            if (Math.Abs(drag_distance.X) + Math.Abs(drag_distance.Y) > 10)
+            {
+                map_builder.updateLngLat(drag_distance);
+                //MessageBox.Show($"Dragged {drag_distance.ToString()}");
+            }
+            updateMap();
+            updateLabels();
+        }
+
+        private void mapGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouse_down_loc = e.GetPosition(this);
+        }
+
+        private void updateMap()
+        {
+            for (int i = 0; i < image_grid.Count; ++i)
+            {
+                for (int j = 0; j < image_grid[i].Count; ++j)
+                {
+                    image_grid[i][j].Source = map_builder.getMapTile(i, j);
+                }
+            }
+        }
+    }
+}
---
+++ Map/Map.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Map
+{
+    internal class MapBuilder
+    {
+        private static string API_key = "AIzaSyCcuduMGTk27vlyVhTUJsUdcHKqlVUZSU8";
+        private static HttpClient http = new()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+        };
+        private string sessionID;
+        private int zoom;
+        private Point lngLat;
+        public MapBuilder() {
+            sessionID = "none";
+            zoom = 0;
+            lngLat = new Point(0, 0);
+        }
+
+        public double getLatitude()
+        {
+            return lngLat.Y;
+        }
+
+        public double getLongitude()
+        {
+            return lngLat.X;
+        }
+
+        public int getZoom()
+        {
+            return zoom;
+        }
+
+        //public async void getMapTile()
+        //{
+        //    if (sessionID == "none") ;
+                //sessionID = await http.PostAsync(
+                //    $"'{{\"mapType\": \"streetview\", \"language\": \"en-US\", \"region\": \"CA\"}}' " +
+                //    "- H 'Content-Type: application/json " +
+                //    "https://tile.googleapis.com/v1/createSession?key={API_key}"
+                //);
+        //}
+
+        public void updateZoom(int update_val)
+        {
+            zoom += update_val;
+            if (zoom < 0)
+                zoom = 0;
+            else if (zoom > 22)
+                zoom = 22;
+        }
+
+        public void updateLngLat(Point distance)
+        {
+            double lat_modifier = distance.Y * 360 / (800 * (Math.Pow(2, zoom)));
+            double lng_modifier = -distance.X * 360 / (800 * (Math.Pow(2, zoom)));
+            lngLat.X += lng_modifier;
+            lngLat.Y += lat_modifier;
+            lngLat.X = lngLat.X % 180;
+            lngLat.Y = lngLat.Y % 180;
+
+        }
+
+        public ImageSource getMapTile(int x, int y)
+        {
+            BitmapImage bi3 = new BitmapImage();
+            bi3.BeginInit();
+            if (zoom % 2 == 0)
+                bi3.UriSource = new Uri("\"Images\\Image1.png\"", UriKind.Relative);
+            else
+                bi3.UriSource = new Uri("\"Images\\image2.png\"", UriKind.Relative);
+            bi3.EndInit();
+
+            return bi3;
+        }
+    }
+}
